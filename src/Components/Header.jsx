d579ddf022b9a68570c24de5cbbe648f389b2177
srushh13 <srushtigupte1305@gmail.com>
2025-07-02 00:18:58 +0530

    issue resolved

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return(
         <div className=" flex justify-between items-center px-6  shadow-lg  bg-orange-100 ">
-            <div><img src="/public/assets/logo.png" alt="Logo" className="size-20"></img></div>
+            <div><img src="src/assets/logo.png" alt="Logo" className="size-20"></img></div>
             <div className="flex gap-15 font-semibold text-green-800">
                 <div className="hover:bg-amber-300 rounded-md p-0.5">Home</div>
                 <div className="hover:bg-amber-300 rounded-md ">About</div>
