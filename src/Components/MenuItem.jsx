ce73de4577cbf6003399118ee80c121c5ec25960
srushh13 <srushtigupte1305@gmail.com>
2025-08-07 16:26:15 +0530

    button add

--- src/Components/MenuItem.jsx
+++ src/Components/MenuItem.jsx
@@ -1,7 +1,7 @@
 import {IMG_URL} from "../constants.js"
 const MenuItem = ({isVeg, name, price, description, imageId}) =>{
     return(
-        <div className="flex justify-between max-w-4xl shadow-xl m-auto p-5 rounded-2xl items-center">
+        <div className="flex justify-between max-w-4xl shadow-xl m-auto p-9 rounded-2xl items-center">
             <div className="space-y-2 flex flex-col">
                 <p className="text-sm">{isVeg ? '🟩' : '🟥'}</p>
                 <p className="font-semibold text-lg text-gray-800">{name}</p>
@@ -10,6 +10,10 @@ const MenuItem = ({isVeg, name, price, description, imageId}) =>{
             </div>
             <div className="w-32 h-30 flex-shrink-0">
                 <img src={IMG_URL + imageId } alt="" className="w-full h-full object-cover rounded-lg shadow"/>
+                <div className="relative left-1/6 -top-4">
+                    <button className="text-green-700 bg-white font-bold pt-2 pb-2 pl-7 pr-7 rounded-xl absolute border border-gray-300
+                    hover:bg-green-600 hover:text-white hover:border-none">ADD</button>
+                </div>
                 
             </div>
         </div>  
